d6ae9fda11e153aeb26d01c7cc8bb29a5c6e9659
ms-chanky <152417978+ms-chanky@users.noreply.github.com>
2024-02-21 10:28:44 +0300

    Delete School directory

--- School/students/url.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path("", views.Name),
-    path('adm', views.adm),
-    path('dob', views.dob),
-    path('clas', views.clas)
-]
